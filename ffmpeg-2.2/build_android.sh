11a397c0806ac44d0b201ec04de7e9d595da4fbf
dennishu <dennishu@cienet.com.cn>
2014-04-01 18:47:34 +0800

    add the build script for android platform

---
+++ ffmpeg-2.2/build_android.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+NDK=/home/dennis/android-ndk-r9d
+SYSROOT=$NDK/platforms/android-9/arch-arm/
+TOOLCHAIN=$NDK/toolchains/arm-linux-androideabi-4.8/prebuilt/linux-x86_64
+
+function build_one
+{
+./configure \
+    --prefix=$PREFIX \
+    --enable-shared \
+    --disable-static \
+    --disable-doc \
+    --disable-ffserver \
+    --enable-cross-compile \
+    --cross-prefix=$TOOLCHAIN/bin/arm-linux-androideabi- \
+    --target-os=linux \
+    --arch=arm \
+    --sysroot=$SYSROOT \
+    --extra-cflags="-Os -fpic $ADDI_CFLAGS" \
+    --extra-ldflags="$ADDI_LDFLAGS" \
+    $ADDITIONAL_CONFIGURE_FLAG
+}
+CPU=arm
+PREFIX=$(pwd)/android/$CPU
+ADDI_CFLAGS="-marm"
+build_one
